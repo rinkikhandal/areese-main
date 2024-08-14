6e441f8979cbeb2f99baafbdc746ee23665c30d2
rinki <rinkikhandal2000@gmail.com>
2024-08-14 22:17:17 +0530

    teachers details js

---
+++ source/assets/js/teachers.js
@@ -0,0 +1,85 @@
+const teacherDetails = [
+  {
+    id: 1,
+    name: "Ankit Kumar",
+    subject: "Physics",
+    honorific: "Mr.",
+    about: "Together we will learn new things",
+    image: "ankit-kumar-1.jpg",
+  },
+  {
+    id: 2,
+    name: "Bhupendra Sharma",
+    subject: "Biology",
+    honorific: "Mr.",
+    about: "Together we will learn new things",
+    image: "Bhupendra-Sharma.png",
+  },
+  {
+    id: 3,
+    name: "Gaurav Sharma",
+    subject: "Mathematics",
+    honorific: "Mr.",
+    about: "Together we will learn new things",
+    image: "gaurav-sharma.png",
+  },
+  {
+    id: 4,
+    name: " Mohit Sharma",
+    subject: "Chemistry",
+    honorific: "Mr.",
+    about: "Together we will learn new things",
+    image: "Mohit-Sharma.png",
+  },
+  {
+    id: 5,
+    name: "Rahul Kumar Singh",
+    subject: "Physics",
+    honorific: "Mr.",
+    about: "Together we will learn new things",
+    image: "Rahul-Kumar-Singh.png",
+  },
+  {
+    id: 6,
+    name: "Pradeep Kumar",
+    subject: "Mathematics",
+    honorific: "Mr.",
+    about: "Together we will learn new things",
+    image: "pradeep-sir.png",
+  },
+];
+
+// looping teachers
+
+const teacherDivs = document.querySelectorAll(".advisor-carousel");
+
+// console.log(teacherDiv);
+teacherDivs.forEach((teacherDiv) => {
+  teacherDiv.innerHTML = teacherDetails
+    .map((teacher) => {
+      const { id, name, subject, honorific, about, image } = teacher;
+      return `<div class="advisor-item" data-id="${id}">
+              <div class="info-box">
+                <img src="assets/img/teachers/${image}" alt="Thumb" />
+                <div class="info-title">
+                  <h4>${honorific} ${name}</h4>
+                  <span>${subject} Faculty</span>
+                </div>
+                <div class="overlay">
+                  <div class="box">
+                    <div class="content">
+                      <div class="overlay-content">
+                        <h4>About ${name}</h4>
+                        <p>
+                        ${about}
+                        </p>
+                        <a href="#">Read More</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>`;
+    })
+    .join("");
+});
--- source/jquery-links.php
+++ source/jquery-links.php
@@ -1,18 +1,20 @@
 <script src="assets/js/jquery-1.12.4.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/equal-height.min.js"></script>
-    <script src="assets/js/jquery.appear.js"></script>
-    <script src="assets/js/jquery.easing.min.js"></script>
-    <script src="assets/js/jquery.magnific-popup.min.js"></script>
-    <script src="assets/js/modernizr.custom.13711.js"></script>
-    <script src="assets/js/owl.carousel.min.js"></script>
-    <script src="assets/js/wow.min.js"></script>
-    <script src="assets/js/isotope.pkgd.min.js"></script>
-    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
-    <script src="assets/js/count-to.js"></script>
-    <script src="assets/js/loopcounter.js"></script>
-    <script src="assets/js/jquery.nice-select.min.js"></script>
-    <script src="assets/js/bootsnav.js"></script>
-    <script src="assets/js/main.js"></script>
-    <script src="assets/js/personal.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.8/axios.min.js" integrity="sha512-PJa3oQSLWRB7wHZ7GQ/g+qyv6r4mbuhmiDb8BjSFZ8NZ2a42oTtAq5n0ucWAwcQDlikAtkub+tPVCw4np27WCg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/equal-height.min.js"></script>
+<script src="assets/js/jquery.appear.js"></script>
+<script src="assets/js/jquery.easing.min.js"></script>
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<script src="assets/js/modernizr.custom.13711.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/wow.min.js"></script>
+<script src="assets/js/isotope.pkgd.min.js"></script>
+<script src="assets/js/imagesloaded.pkgd.min.js"></script>
+<script src="assets/js/count-to.js"></script>
+<script src="assets/js/loopcounter.js"></script>
+<script src="assets/js/jquery.nice-select.min.js"></script>
+<script src="assets/js/bootsnav.js"></script>
+<script src="assets/js/main.js"></script>
+<script src="assets/js/personal.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.8/axios.min.js" integrity="sha512-PJa3oQSLWRB7wHZ7GQ/g+qyv6r4mbuhmiDb8BjSFZ8NZ2a42oTtAq5n0ucWAwcQDlikAtkub+tPVCw4np27WCg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<script src="assets/js/teachers.js"></script>
